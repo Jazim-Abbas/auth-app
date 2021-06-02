54ced8151554c9dd8fcad11817b788a789e36edf
RanaJazim <stackdeveloper87@gmail.com>
2021-06-02 02:49:51 -0700

    change the send mail url dynamically based on user selection ..

--- public/javascripts/send-mail.js
+++ public/javascripts/send-mail.js
@@ -1,47 +1,47 @@
 var vm = new Vue({
   el: "#login-app",
   data: {
-    isOwner: false,
+    acountType: "owner",
     credentials: {
       email: "",
-      password: "",
     },
     error: "",
     errors: [],
   },
   methods: {
-    async onLogin() {
-      try {
-        const res = await axios.post(this.loginURL, { ...this.credentials });
-        this.errors = [];
-        this.error = "";
-        let userData = res.data;
-        if (this.isOwner) {
-          userData.isOwner = true;
-        } else {
-          userData.isOwner = false;
-        }
-        window.localStorage.setItem("user", JSON.stringify(userData));
-        console.log(res.data);
-        window.location.href = "/user/account-details.html";
-      } catch (err) {
-        const { response } = err;
+    async onSendMail() {
+      alert(this.sendMailUrl);
 
-        if (response.status === 422) {
-          this.error = "";
-          this.errors = response.data;
-        } else {
-          this.errors = [];
-          this.error = response.data.message;
-        }
+      //   try {
+      //     const res = await axios.post(this.loginURL, { ...this.credentials });
+      //     this.errors = [];
+      //     this.error = "";
 
-        console.log(response);
-      }
+      //     console.log(res.data);
+      //   } catch (err) {
+      //     const { response } = err;
+
+      //     if (response.status === 422) {
+      //       this.error = "";
+      //       this.errors = response.data;
+      //     } else {
+      //       this.errors = [];
+      //       this.error = response.data.message;
+      //     }
+
+      //     console.log(response);
+      //   }
     },
   },
   computed: {
-    loginURL() {
-      return this.isOwner ? "/venue-owner/login" : "/user/login";
+    sendMailUrl() {
+      const urls = {
+        owner: "/owner",
+        user: "/user",
+        healthOfficial: "/healt",
+      };
+
+      return urls[this.acountType];
     },
   },
 });
