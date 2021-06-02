97042f2190fb72662048ff11e1d25b71b50a7b86
RanaJazim <stackdeveloper87@gmail.com>
2021-06-02 03:31:13 -0700

    added layout for forget password screeen ..

---
+++ public/javascripts/forget-password.js
@@ -0,0 +1,53 @@
+var vm = new Vue({
+  el: "#login-app",
+  data: {
+    acountType: "user",
+    credentials: {
+      email: "",
+      password: "",
+      verificationCode: "",
+    },
+    error: "",
+    errors: [],
+  },
+  methods: {
+    async onSendMail() {
+      alert(this.sendMailUrl);
+
+      //   try {
+      //     const res = await axios.post(this.loginURL, { ...this.credentials });
+      //     this.errors = [];
+      //     this.error = "";
+
+      //     console.log(res.data);
+      //   } catch (err) {
+      //     const { response } = err;
+
+      //     if (response.status === 422) {
+      //       this.error = "";
+      //       this.errors = response.data;
+      //     } else {
+      //       this.errors = [];
+      //       this.error = response.data.message;
+      //     }
+
+      //     console.log(response);
+      //   }
+    },
+  },
+  computed: {
+    sendMailUrl() {
+      const urls = {
+        owner: "/owner",
+        user: "/user",
+        healthOfficial: "/healt",
+      };
+
+      return urls[this.acountType];
+    },
+  },
+});
+
+function validateLogin() {
+  return true;
+}
