20bcc5e826bb5fde134949f69d883a8ea6b57d1d
Jazim-Abbas <jazimabbas87@gmail.com>
2021-06-04 13:15:05 +0500

    implemented health official login ..

--- routes/healthOfficial.js
+++ routes/healthOfficial.js
@@ -2,13 +2,13 @@ var express = require('express');
 var router = express.Router();
 const healthOfficial = require("../controlers/healthOfficial")
 const validation = require("../middlewares/validation")
-const { healthOfficialSchema, forgetPasswordSchema, registerSchema, newOfficialSchema } = require("../validations/healthOfficial")
+const { healthOfficialSchema, forgetPasswordSchema, registerSchema, newOfficialSchema, loginHeahlthOfficialSchema } = require("../validations/healthOfficial")
 const authentication = require("../middlewares/authenticateToken")
 
-router.post("/login", validation(healthOfficialSchema), healthOfficial.login)
+router.post("/login", validation(loginHeahlthOfficialSchema), healthOfficial.login)
 router.patch("/update", authentication, healthOfficial.update)
 router.get("/profile", authentication, healthOfficial.show)
-router.post("/register", authentication, validation(registerSchema), healthOfficial.registerHealthOfficial)
+router.post("/register", validation(registerSchema), healthOfficial.registerHealthOfficial)
 router.post("/send-verification-code", validation(forgetPasswordSchema), healthOfficial.sendVerificationCode);
 router.post("/forget-password", validation(forgetPasswordSchema), healthOfficial.forgetPassword);
 router.post("/verify-new-official", validation(newOfficialSchema), healthOfficial.verifyHealthOfficial);
